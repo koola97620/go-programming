7a82d7f4db93e67d37331aefe5f8d173352a2ddb
Jaeyong <koola97620@nate.com>
2021-01-23 23:58:14 +0900

    blog 글쓰기 참고용

--- src/chapter5/outline.go
+++ src/chapter5/outline.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
+	"os"
 )
 
 func main() {
-	//doc, err := html.Parse(os.Stdin)
+	doc, err := html.Parse(os.Stdin)
+	// zero value: 초기값을 할당하지 않고 변수를 만들었을 때 해당 변수가 갖는 값
+	// nil은 포인터, 인터페이스, 맵, 슬라이스, 채널, 함수의 zero value다.
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
+		os.Exit(1)
+	}
+	outline(nil, doc)
 }
 
 func outline(stack []string, n *html.Node) {
